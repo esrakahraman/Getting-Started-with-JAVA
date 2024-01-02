f40ba6b6fa3dd6e22e65bd78c9919913f9f58585
Nelson Parajuli <121667396+nelson2064@users.noreply.github.com>
2024-01-02 17:36:11 +0545

    Update day2.java
    don't use string directly for manipulation in java because strings are immutable in java

--- day2.java
+++ day2.java
@@ -14,13 +14,34 @@ public class day2 {
         System.out.println(name.equals(code));
         */
 
-        //Concatenating String
+        // //Concatenating String
+        // String name = "Alex";
+        // String surname = "Fire";
+        // String fullName = name + " " + surname + " " + 1;
+        // System.out.println(fullName);
+
+        // String fullName2 = name.concat(" ").concat(surname);
+        // System.out.println(fullName2);
+
+
+
+                        // upgraded version
+            // Using StringBuilder for String Manipulation
+        //the reason to use string builder is because in java strings are immutable so whenever you change or do some modification on string a new string will formed that eventually is not good so we,
+        //have a string builder in java which solves this problem now whenever you change the string new string will not formed it gonna changed in same memory
         String name = "Alex";
         String surname = "Fire";
-        String fullName = name + " " + surname + " " + 1;
+
+        // Using StringBuilder for concatenation
+        StringBuilder fullNameBuilder = new StringBuilder();
+        fullNameBuilder.append(name).append(" ").append(surname).append(" ").append(1);
+        String fullName = fullNameBuilder.toString();
         System.out.println(fullName);
 
-        String fullName2 = name.concat(" ").concat(surname);
+        // Using StringBuilder's append method
+        StringBuilder fullNameBuilder2 = new StringBuilder(name);
+        fullNameBuilder2.append(" ").append(surname);
+        String fullName2 = fullNameBuilder2.toString();
         System.out.println(fullName2);
     }
 }
